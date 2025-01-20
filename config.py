7695a16d996a11bb9f40227b2408d4f2aabb2b75
villain-ji <adirajput12309@gmail.com>
2025-01-20 13:41:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vampire674/SITARAXMUSIC",
+    "https://github.com/VampireBotHub/VAMPIRE-BOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
